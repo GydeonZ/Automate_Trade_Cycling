59d5383cdaad5ed7de0fe0b308066d29513ae78d
Shuu <73419225+GydeonZ@users.noreply.github.com>
2026-01-12 13:56:50 +0700

    Initial commit

---
+++ src/main/java/net/shuu/mod/AutomateTradeCyclingClient.java
@@ -0,0 +1,10 @@
+package net.shuu.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AutomateTradeCyclingClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/shuu/mod/AutomateTradeCyclingDataGenerator.java
@@ -0,0 +1,11 @@
+package net.shuu.mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AutomateTradeCyclingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
